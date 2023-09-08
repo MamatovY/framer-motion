05d47f8fd5a6e4665174226f158441f6f5c21348
MamatovY <mamatovyiman28@gmail.com>
2023-09-08 19:00:35 +0600

    finish sticlkyMenu

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,16 @@ import { motion, useTransform } from 'framer-motion';
 const Header = ({ offsetY, scrollY }) => {
     const heightSizes = [250, 50]
     const imageSizes = [100, 35]
+    const fontSizes = ['40px', '20px']
 
+    //Первый аргумент useTransform это значение скролл который меняется
+    //Второй это какие значение должны быть у скролл от и до
+    //Третий это значение css когда в одной точке одно значение когда доходит до финишной точки другое значение
     const height = useTransform(scrollY, offsetY, heightSizes);
     const imgWidth = useTransform(scrollY, offsetY, imageSizes);
-    const fontSize = '40px';
+    const fontSize = useTransform(scrollY, offsetY, fontSizes);
+    const pOpacity = useTransform(scrollY, [20, 150], [1, 0])
+    const btnOpacity = useTransform(scrollY, [135, 300], [0, 1])
 
     return (
         <motion.header
@@ -33,7 +39,7 @@ const Header = ({ offsetY, scrollY }) => {
                 <motion.button
                     style={{
                         marginLeft: 'auto',
-                        // opacity: btnOpacity,
+                        opacity: btnOpacity,
                     }}
                 >
                     Click me!
@@ -42,7 +48,7 @@ const Header = ({ offsetY, scrollY }) => {
             <motion.p
                 style={{
                     marginTop: '30px',
-                    // opacity: pOpacity,
+                    opacity: pOpacity,
                 }}
             >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad odit porro
